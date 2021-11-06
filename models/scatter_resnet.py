01e05a93c7362b61e19a2807c4f56c8159c8d560
deponce <1543493024@qq.com>
2021-11-06 11:13:53 -0400

    fix channel bugs

--- models/scatter_resnet.py
+++ models/scatter_resnet.py
@@ -23,11 +23,10 @@ class BasicBlock(nn.Module):
     expansion = 1
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
-        n_inner_c = (planes-inplanes)//4
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = conv3x3(inplanes+n_inner_c*1, inplanes+n_inner_c*2, stride)
+        self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = conv3x3(inplanes+n_inner_c*2, inplanes+n_inner_c*3)
+        self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
